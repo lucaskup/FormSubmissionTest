59fd00c148e81f12a6ac0e5792033fe6fab14c69
lucaskup <lucas.kup@gmail.com>
2018-03-11 11:10:46 -0300

    Primeiro Commit de código, recebe uma requisição HTTP e exibe os parâmetros recebidos em uma tabela

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+import sys
+from io import TextIOWrapper
+sys.path.append("..")
+
+import pyproj
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    paramList = []
+    if request.values:
+        for key, value in request.values.items():
+            paramList.append((key,value))
+    return render_template('index.html',parametros = paramList)
+
+if __name__=='__main__':
+    app.run(debug=True)
